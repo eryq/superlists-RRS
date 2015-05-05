fb4afc48362f0d6ff7b3e2515344eb46b9195d3b
Eric Leary <eric@reilyrecovery.com>
2015-05-05 13:35:20 -0400

    Cleaned up tests and code for three views, two templates (home page and list page)

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,7 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
 
     return render(request, 'home.html')
@@ -13,6 +11,10 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
     
 
 

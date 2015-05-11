91c6fe0faf1111f2b17496efa84bd3774aa4e7ac
Eric Leary <eric@reilyrecovery.com>
2015-05-11 13:42:50 -0400

    fix a couple of bugs in tests and views to get minimal fucntionalality, then create a urls.py according to convention within lists application, take those lines out of the superlistsurls.py for best practice.

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+)
